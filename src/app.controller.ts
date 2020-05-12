adbf70d72f38d9d4a26fd4517cdf7bd0ebdf1c6f
Idrees Khan <dotnetdreamer@users.noreply.github.com>
2020-05-12 17:16:29 +0400

    added demo code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  @Get('predict')
+  predict(@Query() args: { description }) {
+    if(!args.description) {
+      return 'description must be passed';
+    }
+
+    return this.appService.predict(args.description);
+  }
+
+  @Get('build')
+  build() {
+    return this.appService.build();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@nestjs/common';
 
+import * as fs from 'fs';
+import * as brain from 'brain.js';
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  predict(description) {
+    let data = JSON.parse(fs.readFileSync('./_ml/trained-net-category.json', 'utf8'));
+
+    const network = new brain.recurrent.LSTM();
+    network.fromJSON(data);
+
+    const predictions = network.run<string>(description);
+    return predictions;
+  }
+
+  build() {
+    return this.trainExpenseCategoryMl();
+  }
+
+  async buildExpensesTrainingSet() {
+    let data: any[] = JSON.parse(fs.readFileSync('./_data/expenses.json', 'utf8'));
+
+    const trainingSet = data.map((e) => {
+        const catName = e.category.name;
+        const obj: brain.IRNNTrainingData = {
+            input: e.description, 
+            output: catName
+        };
+        return obj;
+    });
+    return trainingSet;
+  }
+
+  async trainExpenseCategoryMl() {
+    // create configuration for training
+    const config = {
+        iterations: 100000,
+        log: true,
+        logPeriod: 100,
+        layers: [10],
+        errorThresh: 0.00005,
+    };
+    
+    const data = await this.buildExpensesTrainingSet();
+
+    const network = new brain.recurrent.LSTM();
+    network.train(data, config);
+
+    const json = network.toJSON();
+    const jsonStr = JSON.stringify(json);
+    
+    fs.writeFileSync(`_ml/trained-net-category.json`, jsonStr, 'utf8');
+    return 'done';
   }
 }
